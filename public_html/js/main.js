b739a8c1979d5e543e2138c3f873dc67787c0014
sekaiya <sekai.kobayashi@gmail.com>
2013-08-08 12:58:02 +0900

    refactoring javascript

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,19 +1,18 @@
 jQuery(document).ready( function() {
+
 	$("#sidemenus > *").click(function(){
 		$("#sidemenus").animate({
 			marginLeft: "-270px"
 		}, 500 );
+		
 		var pagename = $(this).attr("id");
 		$("#subContensP").load("inc/" + pagename + ".inc");
 	});
-});
-
-$(function() {
-
+	
 	$(".notice").hover(function(){
 		$("#kotowari").toggle();
 	});
+
 });
-
-
+s
 
